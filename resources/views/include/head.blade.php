d543c0ae1561724fa133fdaeafd8164cab168b4f
kimutai Rop <kimrop19@gmail.com>
2024-07-20 14:49:39 +0300

    update

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title>Veenet Africa & Internet Business HTML-5 Template | Homepage 01</title>
+    <title>Veenet Africa & Internet and Business communications services</title>
     <!-- Stylesheets -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
